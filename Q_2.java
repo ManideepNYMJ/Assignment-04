5296e5c8ef3df0c057c572be3b01f8560edc4862
ManideepNYMJ <117345801+ManideepNYMJ@users.noreply.github.com>
2023-02-01 11:41:01 +0530

    Add files via upload

---
+++ Q_2.java
@@ -0,0 +1,29 @@
+
+public class Q_2 {
+
+
+	public static void main(String[] args) {
+		String key = "Assignments";
+		
+		
+		char[] keyArr = key.toLowerCase().toCharArray();
+		
+		int[] alp = new int[26];
+		
+		for(int i=0; i<keyArr.length; i++) {
+			int index = (keyArr[i]) - 97;
+			alp[index]++;
+		}
+		
+		System.out.println("Duplicates: ");
+		for(int i=0; i<alp.length; i++) {
+			if(alp[i] >= 2) {
+				char ch = (char)(i+97);
+				System.out.print(ch + ", ");
+			}
+		}
+	}
+
+}
+
+
